efdb9aa817cb0f65d6098f4c76a560123d4ac80a
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-07-02 20:58:49 +0500

    Add method hash_code
    method hash_code implemented to give the hash of the key

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,8 +1,18 @@
+require 'digest'
 class Hashmap
   def initialize
-    # load factor detrmined how filled buckets can get before resizing means get resize after 75% filled
+    # load factor determines how filled buckets can get before resizing means get resize after 75% filled
     @load_factor = 0.75
     # the capacity of hashmap is basically bucket
     @capacity = 0
   end
+  def hash_code(key)
+    # key = Digest::SHA256.hexdigest(key)
+    hashcode = 0
+    key.each_char{|char| hashcode = 31*hashcode + char.ord}
+    hashcode
+  end
 end
+
+l = Hashmap.new
+puts l.hash_code("abc")
