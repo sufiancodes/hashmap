449f95274404ac462c80f3f7a8fd596d87c63aae
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-07-14 21:31:24 +0500

    Add attribute accessor and array to store data
    attr accessor include for convenience and correct way to handle hashmap data storage implement

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,9 +1,11 @@
 class Hashmap
+  attr_accessor :load_factor, :capacity, :buckets
   def initialize
     # load factor determines how filled buckets can get before resizing means get resize after 75% filled
     @load_factor = 0.75
-    # the capacity of hashmap is basically bucket
-    @capacity = 0
+    # the capacity of hashmap is basically number of the buckets
+    @capacity = 16
+    @buckets = Array.new(@capacity)
   end
   def hash(key)
     hash_code = 0
@@ -11,7 +13,8 @@ class Hashmap
     hash_code
   end
   def set(key, value)
-    
+    key_index = hash(key)
+
   end
 end
 
