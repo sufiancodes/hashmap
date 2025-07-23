99220bacc1e590bdea8e6de2f9fdc17693b8ddb2
Sufian <176370395+sufiancodes@users.noreply.github.com>
2025-07-23 21:28:16 +0500

    All methods except hash remove node file remove
    most methods except the one use for hash remove and node file remove for now create hashmap linked list way

--- lib/hashmap.rb
+++ lib/hashmap.rb
@@ -1,5 +1,4 @@
-require_relative 'node'
-class Hashmap < Node
+class Hashmap
   attr_accessor :load_factor, :capacity, :buckets, :keys
   def initialize
     # load factor determines how filled buckets can get before resizing means get resize after 75% filled
@@ -7,91 +6,14 @@ class Hashmap < Node
     # the capacity of hashmap is basically number of the buckets
     @capacity = 16
     @buckets = Array.new(@capacity)
-    @keys = []
   end
   def hash(key)
     hash_code = 0
     key.each_char{|char| hash_code = 31*hash_code + char.ord}
     hash_code
   end
-  def set(key, value)
-    resize_if_need
-    keys << key
-    # keys.push(key)
-    key_index = hash(key)%capacity
-    if buckets[key_index] != nil 
-      old = buckets[key_index]
-      buckets[key_index] = Node.new
-      buckets[key_index].key = key_index
-      buckets[key_index].value = value
-      buckets[key_index].next_node = old
-    else
-      buckets[key_index] = value
-    end
-  end
-  def calculate_bucket_size
-    total_filled = 0
-    @buckets.each do |bucket|
-      total_filled += 1 if bucket != nil
-    end
-    total_filled
-  end
-  def calculate_total_entries
-    total_entry = load_factor * capacity
-    total_entry.to_i
-  end
-  def resize_if_need
-    total_filled = calculate_bucket_size
-    total_entries = calculate_total_entries
-    if total_filled >= total_entries
-      @capacity = capacity * 2
-    end
-  end
-  def get(key)
-    key_index = hash(key)%capacity
-    p key_index
-    if key_index != nil
-      return buckets[key_index]
-    end
-  end
-  def has(key)
-    key_index = hash(key)%capacity
-    if buckets[key_index] == nil
-      return false
-    else
-      return true
-    end
-  end
-  def remove(key)
-    key_index = hash(key)%capacity
-    return nil if buckets[key_index] == nil
-    deleted_item = buckets[key_index]
-    buckets[key_index] = nil
-    return deleted_item
-  end
-  def length
-    length = calculate_bucket_size
-    return length
-  end
-  def clear
-    buckets.each_with_index do |value, index|
-      buckets[index] = nil
-    end
-  end
-  def give_keys
-    return keys
-  end
-  def values
-    array = []
-    buckets.each do |value|
-      if value != nil
-        array << value
-      end
-    end
-    return array
-  end
+  
 end
-
 test = Hashmap.new
 # p test.buckets
 test.set('apple', 'red')
@@ -124,4 +46,7 @@ test.set('ls', 'goldenay')
 # length = test.length
 # p length
 n = test.give_keys
+m = test.values
 p n
+p m
+test.entries
--- lib/node.rb
+++
@@ -1,8 +0,0 @@
-class Node
-  attr_accessor :key, :value, :next_node
-  def initialize
-    @key = key
-    @value = value
-    @next_node = nil
-  end
-end
